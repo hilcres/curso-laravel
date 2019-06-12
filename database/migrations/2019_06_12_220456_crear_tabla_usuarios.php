a7e878e0ce2cbd1f5d1f3dc3b7e3daac1a6dfd53
hilcres <hilcres26@gmail.com>
2019-06-12 18:06:44 -0400

    Se vuelve a modificar tabla usuarios para ver si se corrigue el error

--- database/migrations/2019_06_12_220456_crear_tabla_usuarios.php
+++ database/migrations/2019_06_12_220456_crear_tabla_usuarios.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaUsuario extends Migration
+class CrearTablaUsuarios extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CrearTablaUsuario extends Migration
     public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->Increments('id');
             $table->string('nombre' , 50);
             $table->string('password' , 100);
             $table->string('usuario' , 50);
